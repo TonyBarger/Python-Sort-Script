735bb2ad6831f2d166e82c43a2de7caa083a99a5
TonyBarger <112129201+TonyBarger@users.noreply.github.com>
2022-09-28 16:16:18 -0400

    Add files via upload
    Edit to the original program to have a method that decides if the user wants to use the normal sort, or the reverse sort. Also added a line to print "Done" when the results are posted so the user can know if the program actually ran properly from the cmd interface.

--- SortingScript.py
+++ SortingScript.py
@@ -9,15 +9,22 @@ for line in f:
     arri=strip_lines.strip("\n")
     m=arr.append(arri)
 f.close()
+x = input("1 for normal, 2 for Reverse Sort. Enter:")
+if x == 1:
+        print("Normal Sorting..")
+        arr.sort()
+        arr.sort(key=len)
 
-arr.sort()
-arr.sort(key=len)
-
+elif x == 2:
+        print("Reverse Sorting...")
+        arr.sort(reverse=True)
+        arr.sort(key=len, reverse=True)
 
 f=open('result.txt', 'w')
 
 for i in arr:
     f.write('%s\n' % i)
 
+print("Done")
 f.close()
 
